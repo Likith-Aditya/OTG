ca675e5859fafb1c43b2e7b63b2ae65e6a4a290f
Likith <karapakula.likith@gmail.com>
2020-12-15 16:52:45 +0800

    fixed glitch

--- Ounces to grams/script.js
+++ Ounces to grams/script.js
@@ -6,7 +6,7 @@ document.querySelector('.convert1').addEventListener('click', function () {
   document.querySelector('#ounces1').value = ounces;
 });
 document.querySelector('.convert2').addEventListener('click', function () {
-  let ounces2 = Number(document.querySelector('#grams2').value);
+  let ounces2 = Number(document.querySelector('#ounce2').value);
   let grams2 = (ounces2 * 28.35).toFixed(2);
   document.querySelector('#grams2').value = grams2;
 });
